083b5979f9363541f38f993265351c9ad9700aca
henry huntington <henryxhuntington@gmail.com>
2022-10-01 16:48:08 -0700

    Final changes

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-// grab DOM elements
+// set variables
+
+const nameDisplay = document.getElementById('name-display');
+const button = document.getElementById('button');
+const nameInput = document.getElementById('name-input');
 
 // set event listeners
+
+button.addEventListener('click', () => {
+    const name = nameInput.value;
+    nameDisplay.textContent = name;
+})
